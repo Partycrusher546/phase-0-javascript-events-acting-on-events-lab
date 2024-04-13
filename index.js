ece5846a85ad915abb3631df65baa9c563aa3198
partycrusher546 <muthonidennis546@gmail.com>
2024-04-14 00:58:50 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const DODGER = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const left = positionToInteger(DODGER.style.left);
+  if (left > 0) {
+    DODGER.style.left = `${left - 4}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const left = positionToInteger(DODGER.style.left);
+  if (left < 360) {
+    DODGER.style.left = `${left + 4}px`;
+  }
+}
+
+function positionToInteger(p) {
+  return parseInt(p.split('px')[0]) || 0;
+}
